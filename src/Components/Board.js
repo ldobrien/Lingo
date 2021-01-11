9b7975b59be135ffbf8a1f37c863ccf13cf8b06b
Lisa <lisaobrien2009@hotmail.com>
2021-01-10 22:35:16 -0500

    Lingo

---
+++ src/Components/Board.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import {Row, Col} from 'react-materialize'
+import './Board.css'
+
+class Board extends Component {
+    state ={
+        answer: this.props.answer ? this.props.answer : 5,
+        length: this.props.answer.length ? this.props.answer.length : 5,
+        height: 5,
+        guesses: this.props.guesses
+    }
+
+    createBoard = () => {
+        let board = []
+        for(var row = 0; row < this.state.height; row++){
+            let rowArr = []
+            let rowGuess = this.state.guesses[row]
+            for(var col = 0; col < this.state.length; col++){
+                if(rowGuess){
+                    let correctLetter = rowGuess.correctLetter.includes(col);
+                    let correctPosition = rowGuess.correctPosition.includes(col)
+                    let classname = correctPosition ? "greenSquare" : correctLetter ? "yellowCircle" : "white"
+                    classname = classname + " cell"
+                    rowArr.push(<Col className={classname} key={rowArr.length}>
+                        <p className="p">{rowGuess.guess[col]}</p>
+                        </Col>)
+                } else {
+                    rowArr.push(<Col className="cell"><p className="white-text">-</p></Col>)
+                }
+                
+            }
+            board.push(<Row className="row">{rowArr}</Row>)
+        }
+        return board
+    }
+
+    componentDidUpdate(){
+        if(this.state.guesses !== this.props.guesses){
+            this.setState({
+                ...this.state,
+                guesses: this.props.guesses
+            })
+        }
+    }
+
+    render() {
+        let board = this.createBoard()
+        return(
+            <div className="center">{board}</div>
+        )
+    }
+}
+
+export default Board
---
+++ src/Components/Main.js
@@ -0,0 +1,125 @@
+import React, {Component} from 'react'
+import Board from './Board'
+import './Board.css'
+
+class Main extends Component {
+    state ={
+        answer: "",
+        guesses: [],
+        guess: "",
+        length: 5,
+        attempts: 5,
+        result: ""
+    }
+
+    getData = () => {
+        fetch('data.json'
+        ,{
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
+          })
+          .then(res => {
+            return res.json();
+          })
+          .then(json => {
+            var item = json[Math.floor(Math.random() * json.length)];
+            this.setState({
+                answer: item
+            })
+          });
+    }
+
+    componentDidMount(){
+        this.getData()
+    }
+
+    addGuess = (e) => {
+        e.preventDefault()
+        let guess = this.state.guess.toLowerCase()
+        let correctPosition = []
+        let correctLetter = []
+        for(var idx = 0; idx < this.state.length; idx++){
+            if(this.state.answer.includes(guess[idx])){
+                if(guess[idx] === this.state.answer[idx]){
+                    correctPosition.push(idx)
+                } else {
+                    correctLetter.push(idx)
+                }
+            }
+        }
+        let guessObj = {
+            guess,
+            correctPosition,
+            correctLetter
+        }
+        let newGuesses = [...this.state.guesses, guessObj]
+        this.setState({
+            guesses: newGuesses,
+            guess: ""
+        }, () => {
+            if(this.state.length === correctPosition.length){
+                this.handleWin()
+            } else if (this.state.attempts === this.state.guesses.length){
+                this.handleLoss()
+            }
+        })
+    }
+
+    handleWin = () => {
+        this.setState({
+            ...this.state,
+            result: "You Win!"
+        })
+    }
+
+    handleLoss = () => {
+        this.setState({
+            ...this.state,
+            result: "You Lose"
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            guess: e.target.value
+        })
+    }
+
+    playAgain = () => {
+        this.setState({
+            guesses: [],
+            guess: "",
+            length: 5,
+            attempts: 5,
+            result: ""
+        })
+        this.getData()
+    }
+
+    render() {
+        let replay = <div></div>
+        if(this.state.result !== ""){
+            replay = <button onClick={this.playAgain}>Play Again</button>
+        }
+        return(
+            <div>
+                <p className="inputText">{this.state.answer[0]}_ _ _ _</p>
+                
+                <Board answer={this.state.answer} guesses={this.state.guesses}/>
+                <form className="center"  onSubmit={this.addGuess}>
+                    <label>
+                        Guess:
+                        <input type="text" guess="guess" onChange={this.handleChange} minLength={this.state.length} maxLength={this.state.length}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <p className={this.state.result === "You Win!" ? "green-text" : "red-text"}>{this.state.result}</p>
+                {replay}
+            </div>
+        )
+    }
+}
+
+export default Main
